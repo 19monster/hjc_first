6700d80a4dbe1899e7f4171ceaca01a411886d1b
19monster <130068603+19monster@users.noreply.github.com>
2024-11-24 21:51:38 +0800

    first commit

---
+++ assets/AboutView-DsXiBvTM.js
@@ -0,0 +1,1 @@
+import{c as a,a as t,b as o,F as r,o as s,_ as n}from"./index-D3VNUx_R.js";const l={__name:"AboutView",setup(_){return(c,e)=>(s(),a(r,null,[t(n),e[0]||(e[0]=o(" 关于 "))],64))}};export{l as default};
